2529e5c468277ff2e743c62e335fcd9769cc6cc9
fetaro <fetaro@gmail.com>
2012-10-13 10:52:13 +0900

    flymake ruby

--- init.el
+++ init.el
@@ -102,14 +102,6 @@
 (autoload 'php-mode "php-mode" )
 (setq auto-mode-alist
       (cons '("\\.php\\'" . php-mode) auto-mode-alist))
-
-;; ruby-mode
-(autoload 'ruby-mode "ruby-mode" )
-(setq auto-mode-alist
-      (cons '("\\.rb\\'" . ruby-mode) auto-mode-alist))
-(require 'ruby-mode)
-(defun ruby-mode-set-encoding () ())
-
 ;;; js2-mode
 (autoload 'js2-mode "js2" nil t)
 (add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
@@ -198,3 +190,51 @@
   (message "My JS2 hook"))
 
 (add-hook 'js2-mode-hook 'my-js2-mode-hook)
+
+
+;;;;;; flymake for ruby
+
+;; ruby-mode
+;(autoload 'ruby-mode "ruby-mode" )
+;(setq auto-mode-alist
+;      (cons '("\\.rb\\'" . ruby-mode) auto-mode-alist))
+;(require 'ruby-mode)
+;(defun ruby-mode-set-encoding () ())
+
+(require 'flymake)
+;; Invoke ruby with '-c' to get syntax checking
+(defun flymake-ruby-init ()
+  (let* ((temp-file   (flymake-init-create-temp-buffer-copy
+                       'flymake-create-temp-inplace))
+         (local-file  (file-relative-name
+                       temp-file
+                       (file-name-directory buffer-file-name))))
+    (list "ruby" (list "-c" local-file))))
+(push '(".+\\.rb$" flymake-ruby-init) flymake-allowed-file-name-masks)
+(push '("Rakefile$" flymake-ruby-init) flymake-allowed-file-name-masks)
+(push '("^\\(.*\\):\\([0-9]+\\): \\(.*\\)$" 1 2 nil 3) flymake-err-line-patterns)
+(add-hook
+ 'ruby-mode-hook
+ '(lambda ()
+    ;; Don't want flymake mode for ruby regions in rhtml files
+    (if (not (null buffer-file-name)) (flymake-mode))
+    ;; C-c d at error line , show error to minibuffer
+    (define-key ruby-mode-map "\C-cd" 'credmp/flymake-display-err-minibuf)))
+
+(defun credmp/flymake-display-err-minibuf ()
+  "Displays the error/warning for the current line in the minibuffer"
+  (interactive)
+  (let* ((line-no             (flymake-current-line-no))
+         (line-err-info-list  (nth 0 (flymake-find-err-info flymake-err-info line-no)))
+         (count               (length line-err-info-list))
+         )
+    (while (> count 0)
+      (when line-err-info-list
+        (let* ((file       (flymake-ler-file (nth (1- count) line-err-info-list)))
+               (full-file  (flymake-ler-full-file (nth (1- count) line-err-info-list)))
+               (text (flymake-ler-text (nth (1- count) line-err-info-list)))
+               (line       (flymake-ler-line (nth (1- count) line-err-info-list))))
+          (message "[%s] %s" line text)
+          )
+        )
+      (setq count (1- count)))))
