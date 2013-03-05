cec8c04b938dcc892bd9e467bc2824cc20431a1a
fetaro <fetaro@gmail.com>
2013-03-05 22:26:01 +0900

    add coffee-mode

--- init.el
+++ init.el
@@ -54,6 +54,18 @@
 ;; minibuf-isearch
 ;(require 'minibuf-isearch nil t)
 
+;; coffee-mode
+(require 'coffee-mode)
+(add-to-list 'auto-mode-alist '("\\.coffee$" . coffee-mode))
+(defun coffee-custom ()
+  "coffee-mode-hook"
+  (and (set (make-local-variable 'tab-width) 2)
+       (set (make-local-variable 'coffee-tab-width) 2))
+  )
+
+(add-hook 'coffee-mode-hook
+  '(lambda() (coffee-custom)))
+
 ;;-------------------------
 ;; key map
 ;;-------------------------
