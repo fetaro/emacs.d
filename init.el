eff38d8ef11ec896da1e71c7b0377148e8997454
fetaro <fetaro@gmail.com>
2014-09-10 09:30:44 +0900

    add php indent setting

--- init.el
+++ init.el
@@ -213,6 +213,20 @@
 (setq auto-mode-alist
       (cons '("\\.php\\'" . php-mode) auto-mode-alist))
 
+; pretty php indent
+(add-hook 'php-mode-hook (lambda ()
+    (defun ywb-php-lineup-arglist-intro (langelem)
+      (save-excursion
+        (goto-char (cdr langelem))
+        (vector (+ (current-column) c-basic-offset))))
+    (defun ywb-php-lineup-arglist-close (langelem)
+      (save-excursion
+        (goto-char (cdr langelem))
+        (vector (current-column))))
+    (c-set-offset 'arglist-intro 'ywb-php-lineup-arglist-intro)
+    (c-set-offset 'arglist-close 'ywb-php-lineup-arglist-close)))
+
+
 ;;----------
 ;; javascript
 ;;----------
