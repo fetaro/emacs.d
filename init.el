5d1ae5e3b8c6a13f8d57cbfb878088ab3381c3c7
fetaro <fetaro@gmail.com>
2015-07-04 15:26:37 +0900

    change js2-mode

--- init.el
+++ init.el
@@ -260,79 +260,79 @@
     (c-set-offset 'arglist-close 'ywb-php-lineup-arglist-close)))
 
 
-;;----------
-;; javascript
-;;----------
+;; ;;----------
+;; ;; javascript
+;; ;;----------
 
-;;; js2-mode
-(autoload 'js2-mode "js2" nil t)
-(add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
-(add-hook 'js2-mode-hook 'js-indent-hook)
+;; ;;; js2-mode
+;; (autoload 'js2-mode "js2" nil t)
+;; (add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
+;; (add-hook 'js2-mode-hook 'js-indent-hook)
 
-;; pretty js2mode indent
-; refer to http://mihai.bazon.net/projects/editing-javascript-with-emacs-js2-mode
-(defun my-js2-indent-function ()
-  (interactive)
-  (save-restriction
-    (widen)
-    (let* ((inhibit-point-motion-hooks t)
-           (parse-status (save-excursion (syntax-ppss (point-at-bol))))
-           (offset (- (current-column) (current-indentation)))
-           (indentation (espresso--proper-indentation parse-status))
-           node)
-      (save-excursion
-        (back-to-indentation)
-        (if (looking-at "case\\s-")
-            (setq indentation (+ indentation (/ espresso-indent-level 2))))
-        (setq node (js2-node-at-point))
-        (when (and node
-                   (= js2-NAME (js2-node-type node))
-                   (= js2-VAR (js2-node-type (js2-node-parent node))))
-          (setq indentation (+ 4 indentation))))
-      (indent-line-to indentation)
-      (when (> offset 0) (forward-char offset)))))
-(defun my-indent-sexp ()
-  (interactive)
-  (save-restriction
-    (save-excursion
-      (widen)
-      (let* ((inhibit-point-motion-hooks t)
-             (parse-status (syntax-ppss (point)))
-             (beg (nth 1 parse-status))
-             (end-marker (make-marker))
-             (end (progn (goto-char beg) (forward-list) (point)))
-             (ovl (make-overlay beg end)))
-        (set-marker end-marker end)
-        (overlay-put ovl 'face 'highlight)
-        (goto-char beg)
-        (while (< (point) (marker-position end-marker))
-          (beginning-of-line)
-          (unless (looking-at "\\s-*$")
-            (indent-according-to-mode))
-          (forward-line))
-        (run-with-timer 0.5 nil '(lambda(ovl)
-                                   (delete-overlay ovl)) ovl)))))
-(defun my-js2-mode-hook ()
-  (require 'espresso)
-  (setq espresso-indent-level 4
-        indent-tabs-mode nil
-        c-basic-offset 4)
-  (c-toggle-auto-state 0)
-  (c-toggle-hungry-state 1)
-  (set (make-local-variable 'indent-line-function) 'my-js2-indent-function)
-  (define-key js2-mode-map "\C-\M-\\"
-    '(lambda()
-       (interactive)
-       (insert "/* -----[ ")
-       (save-excursion
-         (insert " ]----- */"))
-       ))
-  (define-key js2-mode-map "\C-m" 'newline-and-indent)
-  (define-key js2-mode-map "\C-\M-q" 'my-indent-sexp)
-  (if (featurep 'js2-highlight-vars)
-      (js2-highlight-vars-mode))
-  (message "My JS2 hook"))
-(add-hook 'js2-mode-hook 'my-js2-mode-hook)
+;; ;; pretty js2mode indent
+;; ; refer to http://mihai.bazon.net/projects/editing-javascript-with-emacs-js2-mode
+;; (defun my-js2-indent-function ()
+;;   (interactive)
+;;   (save-restriction
+;;     (widen)
+;;     (let* ((inhibit-point-motion-hooks t)
+;;            (parse-status (save-excursion (syntax-ppss (point-at-bol))))
+;;            (offset (- (current-column) (current-indentation)))
+;;            (indentation (espresso--proper-indentation parse-status))
+;;            node)
+;;       (save-excursion
+;;         (back-to-indentation)
+;;         (if (looking-at "case\\s-")
+;;             (setq indentation (+ indentation (/ espresso-indent-level 2))))
+;;         (setq node (js2-node-at-point))
+;;         (when (and node
+;;                    (= js2-NAME (js2-node-type node))
+;;                    (= js2-VAR (js2-node-type (js2-node-parent node))))
+;;           (setq indentation (+ 4 indentation))))
+;;       (indent-line-to indentation)
+;;       (when (> offset 0) (forward-char offset)))))
+;; (defun my-indent-sexp ()
+;;   (interactive)
+;;   (save-restriction
+;;     (save-excursion
+;;       (widen)
+;;       (let* ((inhibit-point-motion-hooks t)
+;;              (parse-status (syntax-ppss (point)))
+;;              (beg (nth 1 parse-status))
+;;              (end-marker (make-marker))
+;;              (end (progn (goto-char beg) (forward-list) (point)))
+;;              (ovl (make-overlay beg end)))
+;;         (set-marker end-marker end)
+;;         (overlay-put ovl 'face 'highlight)
+;;         (goto-char beg)
+;;         (while (< (point) (marker-position end-marker))
+;;           (beginning-of-line)
+;;           (unless (looking-at "\\s-*$")
+;;             (indent-according-to-mode))
+;;           (forward-line))
+;;         (run-with-timer 0.5 nil '(lambda(ovl)
+;;                                    (delete-overlay ovl)) ovl)))))
+;; (defun my-js2-mode-hook ()
+;;   (require 'espresso)
+;;   (setq espresso-indent-level 4
+;;         indent-tabs-mode nil
+;;         c-basic-offset 4)
+;;   (c-toggle-auto-state 0)
+;;   (c-toggle-hungry-state 1)
+;;   (set (make-local-variable 'indent-line-function) 'my-js2-indent-function)
+;;   (define-key js2-mode-map "\C-\M-\\"
+;;     '(lambda()
+;;        (interactive)
+;;        (insert "/* -----[ ")
+;;        (save-excursion
+;;          (insert " ]----- */"))
+;;        ))
+;;   (define-key js2-mode-map "\C-m" 'newline-and-indent)
+;;   (define-key js2-mode-map "\C-\M-q" 'my-indent-sexp)
+;;   (if (featurep 'js2-highlight-vars)
+;;       (js2-highlight-vars-mode))
+;;   (message "My JS2 hook"))
+;; (add-hook 'js2-mode-hook 'my-js2-mode-hook)
 
 ;;-----------
 ;; ruby
