741d7fe1bc4c054d863e92224126c87265f73c5e
Tetsutaro Watanabe <w_tetsutaro@r.recruit.co.jp>
2016-04-13 01:08:59 +0900

    add flymake

--- init.el
+++ init.el
@@ -470,3 +470,28 @@ This is particularly useful under Mac OSX, where GUI apps are not started from a
     (((class color) (background dark))
      (:foreground "white" :underline "white" :weight bold)))
   "Face for level-4 headers.")
+
+;;------------
+;; python
+;;------------
+
+(add-hook 'find-file-hook 'flymake-find-file-hook)
+(when (load "flymake" t)
+  (defun flymake-pyflakes-init ()
+    (let* ((temp-file (flymake-init-create-temp-buffer-copy
+                       'flymake-create-temp-inplace))
+           (local-file (file-relative-name
+                        temp-file
+                        (file-name-directory buffer-file-name))))
+      (list "pyflakes"  (list local-file))))
+  (add-to-list 'flymake-allowed-file-name-masks
+               '("\\.py\\'" flymake-pyflakes-init)))
+; show message on mini-buffer
+(defun flymake-show-help ()
+  (when (get-char-property (point) 'flymake-overlay)
+    (let ((help (get-char-property (point) 'help-echo)))
+      (if help (message "%s" help)))))
+(add-hook 'post-command-hook 'flymake-show-help)
+
+
+
