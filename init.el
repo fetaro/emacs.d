8c4b3fdd3cc92878c0195fa8b62c90641d256d19
Tetsutaro Watanabe <w_tetsutaro@r.recruit.co.jp>
2016-10-26 11:34:11 +0900

    R-Mac

--- init.el
+++ init.el
@@ -264,14 +264,6 @@ This is particularly useful under Mac OSX, where GUI apps are not started from a
 (ad-enable-advice 'font-lock-mode 'before 'my-font-lock-mode)
 (ad-activate 'font-lock-mode)
 
-;;-------------------------
-;; font
-;;-------------------------
-(set-face-attribute 'default nil :family "MS Mincho" :height 140)
-;(set-face-attribute 'default nil :family "MS Gothic" :height 140)
-;(set-face-attribute 'default nil :family "MS PMincho" :height 140)
-;(set-face-attribute 'default nil :family "MS PGothic" :height 140)
-
 ;; ------------------------------
 ;; prevent from killing *scratch* buffer
 ;; ------------------------------
@@ -471,27 +463,12 @@ This is particularly useful under Mac OSX, where GUI apps are not started from a
      (:foreground "white" :underline "white" :weight bold)))
   "Face for level-4 headers.")
 
-;;------------
-;; python
-;;------------
-
-(add-hook 'find-file-hook 'flymake-find-file-hook)
-(when (load "flymake" t)
-  (defun flymake-pyflakes-init ()
-    (let* ((temp-file (flymake-init-create-temp-buffer-copy
-                       'flymake-create-temp-inplace))
-           (local-file (file-relative-name
-                        temp-file
-                        (file-name-directory buffer-file-name))))
-      (list "pyflakes"  (list local-file))))
-  (add-to-list 'flymake-allowed-file-name-masks
-               '("\\.py\\'" flymake-pyflakes-init)))
-; show message on mini-buffer
-(defun flymake-show-help ()
-  (when (get-char-property (point) 'flymake-overlay)
-    (let ((help (get-char-property (point) 'help-echo)))
-      (if help (message "%s" help)))))
-(add-hook 'post-command-hook 'flymake-show-help)
 
 
 
+;;--------------
+;; Python
+;;--------------
+
+(require 'flymake-python-pyflakes)
+(add-hook 'python-mode-hook 'flymake-python-pyflakes-load)
--- os/linux.el
+++ os/linux.el
@@ -1,2 +1,10 @@
 ;; Linux
 
+;;-------------------------
+;; font
+;;-------------------------
+(set-face-attribute 'default nil :family "MS Mincho" :height 140)
+;(set-face-attribute 'default nil :family "MS Gothic" :height 140)
+;(set-face-attribute 'default nil :family "MS PMincho" :height 140)
+;(set-face-attribute 'default nil :family "MS PGothic" :height 140)
+
