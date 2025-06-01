09bc5c1e20c3d19dcde90e4c9fd7e86b020697d1
fetaro <tetsutaro.watanabe@goinc.jp>
2025-06-01 23:32:18 +0900

    フォントのバグを修正

--- init.el
+++ init.el
@@ -151,14 +151,6 @@
      (color-theme-arjen)))
 ;; font-lock-keyword-face
 
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(default ((t (:family "DejaVu Sans Mono" :foundry "PfEd" :slant normal :weight normal :height 98 :width normal))))
- '(font-lock-keyword-face ((t (:foreground "cyan" :bold t)))))
-
 
 ;; Show TAB and ZENKAKU space
 ;"　" <- ZENKAKU space
